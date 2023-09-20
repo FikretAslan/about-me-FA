a6485add65436aef4697180c3954bd310b16cebe
Fikret Aslan <f.aslan0191@gmail.com>
2023-09-20 14:09:34 +0100

    changes

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 let username = prompt("What is your name?");
 alert("Well hello there " + username + " and welcome to my website!");
 
-let ans = prompt("Do I have a degree in Sociology? (yes/no)").toLowerCase();
-while (ans !== "yes") {
-  ans = prompt(
-    "It must be yes or no. Do I have a degree in Sociology?"
-  ).toLowerCase();
+let ans = prompt("Do I have a degree in Sociology? (yes/no)")
+  .toLowerCase()
+  .trim();
+while (ans !== "yes" && ans !== "no") {
+  ans = prompt("It must be yes or no. Do I have a degree in Sociology?")
+    .toLowerCase()
+    .trim();
 }
 
 if (ans === "yes") {
+  alert("You are ABSOLUTELY correct!");
+} else {
+  alert("Nope, nice try though!");
+}
+
+let ans2 = prompt("Do I love chicken and chips? (yes/no)").toLowerCase().trim();
+while (ans2 !== "yes" && ans2 !== "no") {
+  ans2 = prompt("It must be yes or no!").toLowerCase().trim();
+}
+if (ans2 === "yes") {
+  alert("You are indeed correct!");
+} else {
+  alert("Nope! Good try though.");
+}
+
+let ans3 = prompt("Did I visit Bali for my honeymoon? (yes/no)")
+  .toLowerCase()
+  .trim();
+while (ans3 !== "yes" && ans3 !== "no") {
+  ans3 = prompt("It must be yes or no!").toLowerCase().trim();
+}
+
+if (ans3 === "no") {
+  alert("You are ABSOLUTELY correct!");
+} else {
+  alert("Nope, nice try though!");
+}
+
+let ans4 = prompt("Am I a huge fan of World of Warcraft? (yes/no)")
+  .toLowerCase()
+  .trim();
+while (ans4 !== "yes" && ans4 !== "no") {
+  ans4 = prompt("It must be yes or no!").toLowerCase().trim();
+}
+
+if (ans4 === "yes") {
+  alert("You are ABSOLUTELY correct!");
+} else {
+  alert("Nope, nice try though!");
+}
+
+let ans5 = prompt("Marmite? (yes/no)").toLowerCase().trim();
+while (ans5 !== "yes" && ans5 !== "no") {
+  ans = prompt("It must be yes or no!").toLowerCase().trim();
+}
+
+if (ans5 === "no") {
   alert("You are ABSOLUTELY correct!");
 } else {
   alert("Nope, nice try though!");
