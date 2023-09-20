0af56e375cc5fddd9e8c59369cf684531777ad90
Fikret Aslan <f.aslan0191@gmail.com>
2023-09-20 11:29:17 +0100

    changes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 let username = prompt("What is your name?");
 alert("Well hello there " + username + " and welcome to my website!");
+
+let ans = prompt("Do I have a degree in Sociology? (yes/no)").toLowerCase();
+while (ans !== "yes") {
+  ans = prompt(
+    "It must be yes or no. Do I have a degree in Sociology?"
+  ).toLowerCase();
+}
+
+if (ans === "yes") {
+  alert("You are ABSOLUTELY correct!");
+} else {
+  alert("Nope, nice try though!");
+}
